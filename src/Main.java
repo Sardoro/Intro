e413e5bad3f6ebab3aa539fc50b3f71f487742a1
Sardoro <sardor.jumaniyazov.90@mail.ru>
2026-09-07 13:25:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        int i = 0;
+        while (i < 1000000){
+            System.out.print(".");
+            i++;
+        }
+
+        long ende = System.currentTimeMillis();
+
+        long dauer = ende - start;
+
+        System.out.println("Dauer: " + dauer + "ms");
+        System.out.println(start);
+        System.out.println(ende);
+
+    }
+}
